ca27691641de697f4d0b3a651b1754f3a7efd5ab
Alice Hubenko <ahubenko@redhat.com>
2020-08-17 18:52:30 -0700

    Added tests

--- pkg/e2e/operators/managedvelero.go
+++ pkg/e2e/operators/managedvelero.go
@@ -39,17 +39,78 @@ var _ = ginkgo.Describe("[Suite: operators] [OSD] Managed Velero Operator", func
 		[]string{"managed-velero-operator"})
 	checkVeleroBackups(h)
 
-	ginkgo.It("Access should be forbidden to edit Backup", func() {
+	ginkgo.It("Access should be forbidden to edit Backups", func() {
 		h.SetServiceAccount("system:serviceaccount:%s:dedicated-admin-project")
 
 		backup := velerov1.Backup{
 			ObjectMeta: metav1.ObjectMeta{
-				Name: "dedicated-admin-failure-test",
+				Name: "dedicated-admin-backup-test",
 			},
 		}
-		_, err := h.Velero().VeleroV1().Backups(h.CurrentProject()).Create(context.TODO(), &backup)
+		_, err := h.Velero().VeleroV1().Backups(h.CurrentProject()).Create(context.TODO(), &backup, metav1.CreateOptions{})
 		Expect(apierrors.IsForbidden(err)).To(BeTrue())
 	})
+
+	ginkgo.It("Access should be forbidden to edit DeleteBackupRequests", func() {
+		h.SetServiceAccount("system:serviceaccount:%s:dedicated-admin-project")
+
+		delete_backup_request := velerov1.DeleteBackupRequest{
+			ObjectMeta: metav1.ObjectMeta{
+				Name: "dedicated-admin-delete-backup-request-test",
+			},
+		}
+		_, err := h.Velero().VeleroV1().DeleteBackupRequests(h.CurrentProject()).Create(context.TODO(), &delete_backup_request, metav1.CreateOptions{})
+		Expect(apierrors.IsForbidden(err)).To(BeTrue())
+	})
+
+	ginkgo.It("Access should be forbidden to edit BackupStorageLocations", func() {
+		h.SetServiceAccount("system:serviceaccount:%s:dedicated-admin-project")
+
+		backup_storage_location := velerov1.BackupStorageLocation{
+			ObjectMeta: metav1.ObjectMeta{
+				Name: "dedicated-admin-backup-storage-location-test",
+			},
+		}
+		_, err := h.Velero().VeleroV1().BackupStorageLocations(h.CurrentProject()).Create(context.TODO(), &backup_storage_location, metav1.CreateOptions{})
+		Expect(apierrors.IsForbidden(err)).To(BeTrue())
+	})
+
+	ginkgo.It("Access should be forbidden to edit DownloadRequests", func() {
+		h.SetServiceAccount("system:serviceaccount:%s:dedicated-admin-project")
+
+		download_request := velerov1.DownloadRequest{
+			ObjectMeta: metav1.ObjectMeta{
+				Name: "dedicated-admin-download-request-test",
+			},
+		}
+		_, err := h.Velero().VeleroV1().DownloadRequests(h.CurrentProject()).Create(context.TODO(), &download_request, metav1.CreateOptions{})
+		Expect(apierrors.IsForbidden(err)).To(BeTrue())
+	})
+
+	ginkgo.It("Access should be forbidden to edit PodVolumeBackups", func() {
+		h.SetServiceAccount("system:serviceaccount:%s:dedicated-admin-project")
+
+		pod_volume_backup := velerov1.PodVolumeBackup{
+			ObjectMeta: metav1.ObjectMeta{
+				Name: "dedicated-admin-pod-volume-backup-test",
+			},
+		}
+		_, err := h.Velero().VeleroV1().PodVolumeBackups(h.CurrentProject()).Create(context.TODO(), &pod_volume_backup, metav1.CreateOptions{})
+		Expect(apierrors.IsForbidden(err)).To(BeTrue())
+	})
+
+	ginkgo.It("Access should be forbidden to edit PodVolumeRestores", func() {
+		h.SetServiceAccount("system:serviceaccount:%s:dedicated-admin-project")
+
+		pod_volume_restore := velerov1.PodVolumeRestore{
+			ObjectMeta: metav1.ObjectMeta{
+				Name: "dedicated-admin-pod-volume-restore-test",
+			},
+		}
+		_, err := h.Velero().VeleroV1().PodVolumeRestores(h.CurrentProject()).Create(context.TODO(), &pod_volume_restore, metav1.CreateOptions{})
+		Expect(apierrors.IsForbidden(err)).To(BeTrue())
+	})
+
 })
 
 func checkVeleroBackups(h *helper.H) {
